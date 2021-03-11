830191dc2e2d063fa5a40ce399cd7bcb76c62e6a
Bayartsogt Yadamsuren <bayartsogt.yadamsuren@icloud.com>
2021-03-11 16:55:36 -0600

    mouse maze lab

---
+++ js/maze.js
@@ -0,0 +1,38 @@
+// States:
+var wallHit = false;
+var started = false;
+
+var loseFunc = () => {
+    if (started){
+        $(".boundary").addClass("youlose");
+        $("h2").text("Sorry you lost :[");
+        started = false;
+        wallHit = true;
+    }
+}
+
+$(document).ready(() => {
+
+    $("#start").click((e) => { 
+        console.log("started");
+        started = true;
+        wallHit = false;
+        $(".boundary").removeClass("youlose");
+        $("h2").text("Game On!");
+    });
+
+    $("#maze").mouseleave(() => { 
+        loseFunc();
+    });
+
+    $(".boundary").mouseover(() => {
+        loseFunc();
+    });
+
+    $("#end").mouseover(() => {
+        if (started && !wallHit){
+            $("h2").text("You win :]");
+            started = false;
+        }
+    });
+});
